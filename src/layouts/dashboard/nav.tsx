2ac33f900045942c3c6d1e44f2ddd52fe3ade5df
Joao Tapparo <joaotapparo@protonmail.com>
2024-10-28 17:17:14 -0300

    removing some tabs

--- src/layouts/dashboard/nav.tsx
+++ src/layouts/dashboard/nav.tsx
@@ -118,11 +118,7 @@ export function NavContent({ data, slots, workspaces, sx }: NavContentProps) {
 
   return (
     <>
-      <Logo />
-
       {slots?.topArea}
-
-      <WorkspacesPopover data={workspaces} sx={{ my: 2 }} />
 
       <Scrollbar fillContent>
         <Box component="nav" display="flex" flex="1 1 auto" flexDirection="column" sx={sx}>
